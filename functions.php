8c8d149755bdc19f6fd1c318a46be62e4153a0d6
jeremyfelt <jeremy.felt@gmail.com>
2014-11-10 14:25:57 -0800

    Prefix functions that are relatively global

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ function wsu_showcase_script_version() {
 	return spine_get_script_version() . '0.0.1';
 }
 
-add_action( 'wp_enqueue_scripts', 'spine_child_enqueue_scripts', 11 );
+add_action( 'wp_enqueue_scripts', 'wsu_showcase_enqueue_scripts', 11 );
 /**
  * Hooking into wp_enqueue_scripts allows you to add custom Javascript to every front
  * end page view. Using available APIs in WordPress, you can restrict this to
@@ -17,6 +17,6 @@ add_action( 'wp_enqueue_scripts', 'spine_child_enqueue_scripts', 11 );
  *
  * As with the action above, change the prefix of this function from spine_child
  */
-function spine_child_enqueue_scripts() {
+function wsu_showcase_enqueue_scripts() {
 	wp_enqueue_script( 'wsu-showcase', get_stylesheet_directory_uri() . '/js/custom.min.js', array( 'jquery' ), wsu_showcase_script_version(), true );
 }
